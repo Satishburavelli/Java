ca155a869bc1ae7fd484d7ef73c14f20f7473885
Satishburavelli <138653813+Satishburavelli@users.noreply.github.com>
2024-12-23 23:59:39 +0100

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,11 @@
+public class forloop
+{
+    public static void main(String[] args)
+    {
+        int j;
+        for (j=0;j<3;j++)
+        {
+            System.out.println("the values of:" +j);
+        }
+    }
+}
